810484ddf68545985fa86a7023f349815bee0ce1
usman-2000 <usmanrahim2000@gmail.com>
2024-05-06 18:59:52 +0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import TodoTasks from './components/todoTasks';
+
+function App() {
+  return (
+    <div className="">
+      <TodoTasks />
+    </div>
+  );
+}
+
+export default App;
